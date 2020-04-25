d26e268d76d0d45820ddf406f4c545a2594a6b3a
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-04-25 21:02:41 +0545

    inline edit changes

--- network/static/network/likeUnlike.js
+++ network/static/network/likeUnlike.js
@@ -90,7 +90,7 @@ function clickHandler(e){
             target.style.display = "none"
             parent.querySelector("i.far.fa-edit.text-secondary").style.display = "inline-block"
         })
-        .catch(error=>console.log(error))
+        .catch(error=>{return error})
     }
 
 }
